62d150b3eca257a0af016fd762160d111cb9bdaf
Hank Hughes <hank.hughes@sony.com>
2021-12-10 17:16:39 -0800

    Add xaws

--- zsh/configs/utils/sap.zsh
+++ zsh/configs/utils/sap.zsh
@@ -13,21 +13,23 @@ function sap() {
     unset AWS_DEFAULT_PROFILE AWS_PROFILE AWS_EB_PROFILE
     echo AWS profile cleared.
     return
+  else
+    local -a available_profiles
+    available_profiles=($(aws_profiles))
+    if [[ -z "${available_profiles[(r)$1]}" ]]; then
+      echo "${fg[red]}Profile '$1' not found in '${AWS_CONFIG_FILE:-$HOME/.aws/config}'" >&2
+      echo "Available profiles: ${(j:, :)available_profiles:-no profiles found}${reset_color}" >&2
+      return 1
+    fi
   fi
 
-  local -a available_profiles
-  available_profiles=($(aws_profiles))
-  if [[ -z "${available_profiles[(r)$1]}" ]]; then
-    echo "${fg[red]}Profile '$1' not found in '${AWS_CONFIG_FILE:-$HOME/.aws/config}'" >&2
-    echo "Available profiles: ${(j:, :)available_profiles:-no profiles found}${reset_color}" >&2
-    return 1
-  fi
-
+  xaws $1  # Sourced from ~/.gimme-aws-creds-xaws
+           # via https://github.sie.sony.com/sie/gimme-aws-creds
+  
   export AWS_DEFAULT_PROFILE=$1
   export AWS_PROFILE=$1
   export AWS_EB_PROFILE=$1
 }
-
 
 compctl -K _aws_profiles xaws sap
 

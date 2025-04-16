c5f33e163452cd7bff29d229f91cdda0e57e579e
Hank Hughes <hank.hughes@sony.com>
2025-04-15 23:04:18 -0700

    Add python venv

--- zsh/themes/p10k.zsh
+++ zsh/themes/p10k.zsh
@@ -59,7 +59,7 @@
     background_jobs         # presence of background jobs
 #    direnv                  # direnv status (https://direnv.net/)
 #    asdf                    # asdf version manager (https://github.com/asdf-vm/asdf)
-#    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
+    virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
 #    anaconda                # conda environment (https://conda.io/)
 #    pyenv                   # python environment (https://github.com/pyenv/pyenv)
 #    goenv                   # go environment (https://github.com/syndbg/goenv)
@@ -894,7 +894,7 @@
   # Separate environment name from Python version only with a space.
   typeset -g POWERLEVEL9K_VIRTUALENV_{LEFT,RIGHT}_DELIMITER=
   # Custom icon.
-  # typeset -g POWERLEVEL9K_VIRTUALENV_VISUAL_IDENTIFIER_EXPANSION='⭐'
+  typeset -g POWERLEVEL9K_VIRTUALENV_VISUAL_IDENTIFIER_EXPANSION='🐍'
 
   #####################[ anaconda: conda environment (https://conda.io/) ]######################
   # Anaconda environment color.
